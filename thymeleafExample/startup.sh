6e0df955fcf2d18394e22db3d5afd04e062018e9
sanket0802 <93255738+sanket0802@users.noreply.github.com>
2022-12-12 14:21:00 +0530

    Update startup.sh

--- thymeleafExample/startup.sh
+++ thymeleafExample/startup.sh
@@ -1,10 +1,11 @@
-cd /home/admin123/themeleaf/thymeleafExample/
-sudo apt install openjdk-8-jdk -y
-sudo apt-get install maven -y 
-mvn test
-mvn package
-mvn install
-#cd /home/admin123/themeleaf/
-chmod 700 startup.sh
-cd /home/admin123/themeleaf/thymeleafExample/target
-nohup java -jar work-0.0.1-SNAPSHOT.jar >/tmp/app.log 2>&1 &
+#! /bin/bash
+  cd /home/admin123/themeleaf/thymeleafExample/
+  sudo apt install openjdk-8-jdk -y
+  sudo apt-get install maven -y 
+  mvn test
+  mvn package
+  #mvn install
+  #cd /home/admin123/themeleaf/
+  #chmod 700 startup.sh
+  cd /home/admin123/themeleaf/thymeleafExample/target
+  nohup java -jar work-0.0.1-SNAPSHOT.jar >/tmp/app.log 2>&1 &
