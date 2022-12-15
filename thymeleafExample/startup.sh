1eb6877762cd26d3208e8a86bcf0eb2d8de0e78e
sanket0802 <93255738+sanket0802@users.noreply.github.com>
2022-12-15 15:40:40 +0530

    Update startup.sh

--- thymeleafExample/startup.sh
+++ thymeleafExample/startup.sh
@@ -9,4 +9,4 @@
   #cd /home/admin123/themeleaf/
   #chmod 700 startup.sh
   cd target
-  sudo nohup java -jar work-0.0.1-SNAPSHOT.jar >/tmp/app.log 2>&1 &
+  sudo nohup java -jar work-0.0.1-SNAPSHOT.jar 
